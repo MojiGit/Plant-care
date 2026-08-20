fb3de2cbf95842d53fb77aa1842cb009b10b8eb2
Ubuntu <ubuntu@ip-172-31-26-205.eu-north-1.compute.internal>
2026-08-20 19:02:53 +0000

    Fix: add flutter/foundation import for debugPrint in plant_id_service

--- lib/services/plant_id_service.dart
+++ lib/services/plant_id_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:flutter/foundation.dart';
 import 'package:dio/dio.dart';
 import '../config/secrets.dart';
 import '../data/models/plant_result.dart';
